151182678ea7e69d64b4462189697fc05bf747a9
Omar Emad <omarelwardany@outlook.com>
2023-12-22 01:29:23 +0200

    Added rapidjson library

---
+++ XMLtoJSON/xmltojson.cpp
@@ -0,0 +1,12 @@
+#include "./rapidjson/document.h"
+#include "./rapidjson/writer.h"
+#include "./rapidjson/stringbuffer.h"
+#include "./tinyxml2.h"
+
+using namespace rapidjson;
+using namespace std;
+
+string xmlToJson(string xmlString) {
+    // TODO
+    return "WIP";
+}
