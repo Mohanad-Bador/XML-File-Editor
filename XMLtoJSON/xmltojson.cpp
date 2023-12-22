2ff1b83d39d287a2a5a1bd4e99c37364e4371d95
Omar Emad <omarelwardany@outlook.com>
2023-12-22 14:06:18 +0200

    Implemented xmltojson

--- XMLtoJSON/xmltojson.cpp
+++ XMLtoJSON/xmltojson.cpp
@@ -1,15 +1,44 @@
 #include "./rapidjson/document.h"
-#include "./rapidjson/writer.h"
+#include "./rapidjson/prettywriter.h"
 #include "./rapidjson/stringbuffer.h"
-#include "./tinyxml2.h"
+#include "./tinyxml2.cpp"
 #include <string>
 
 using namespace std;
 
+
+/*
+    This function is meant to be called inside xmlToJson only
+    NOT FOR USE
+*/
+void processXmlElement(const tinyxml2::XMLElement* element, rapidjson::Value& jsonElement, rapidjson::Document::AllocatorType& allocator) {
+    // iterate over tag attributes
+    for (const tinyxml2::XMLAttribute* attribute = element->FirstAttribute(); attribute != nullptr; attribute = attribute->Next()) {
+        jsonElement.AddMember(rapidjson::Value().SetString(attribute->Name(), allocator), rapidjson::Value().SetString(attribute->Value(), allocator), allocator);
+    }
+
+    // handle text content
+    const char* text = element->GetText();
+    if (text) {
+        jsonElement.AddMember(rapidjson::Value().SetString("text", allocator), rapidjson::Value().SetString(text, allocator), allocator);
+    }
+
+    // recursion
+    for (const tinyxml2::XMLNode* node = element->FirstChild(); node != nullptr; node = node->NextSibling()) {
+        if (node->ToElement()) {
+            rapidjson::Value childElement(rapidjson::kObjectType);
+            processXmlElement(node->ToElement(), childElement, allocator);
+            jsonElement.AddMember(rapidjson::Value().SetString(node->ToElement()->Name(), allocator), childElement, allocator);
+        }
+    }
+}
+
+
+
 /*
     Convert XML String to JSON String
-    @param xmlString XML content as a string
-    @return JSON content as a string
+    @param xmlString minified XML content as a string
+    @return prettified JSON content as a string
 */
 string xmlToJson(string xmlString) {
     // convert string to xml document
@@ -22,20 +51,16 @@ string xmlToJson(string xmlString) {
     rapidjson::Document jsonDoc;
     jsonDoc.SetObject();
 
-    // Iterate over XML tags
-    for (const tinyxml2::XMLNode* node = doc.FirstChild(); node != nullptr; node = node->NextSibling()) {
-        if (node->ToElement()) {
-            rapidjson::Value jsonElement(rapidjson::kObjectType);
-            // Iterate over tag attributes
-            for (const tinyxml2::XMLAttribute* attribute = node->ToElement()->FirstAttribute(); attribute != nullptr; attribute == attribute->Next()) {
-                jsonElement.AddMember(rapidjson::Value().SetString(attribute->Name()), jsonElement, jsonDoc.GetAllocator()); // TODO: fix IntelliSense warning here
-            }
-            {
-                /* code */
-            }
-            
-        }
+     // get root element
+    const tinyxml2::XMLElement* root = doc.RootElement();
+    if (root) {
+        // recursively process the root element and its children
+        processXmlElement(root, jsonDoc, jsonDoc.GetAllocator());
     }
-    // TODO
-    return "WIP";
+
+    // json to pretty string
+    rapidjson::StringBuffer buffer;
+    rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
+    jsonDoc.Accept(writer);
+    return buffer.GetString();
 }
