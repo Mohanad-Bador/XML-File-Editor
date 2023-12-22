d84292f510d3f73c98f53ae34f21ee11eb8bcd95
Omar Emad <omarelwardany@outlook.com>
2023-12-22 02:09:50 +0200

    Partially implemented XMLtoJSON

--- XMLtoJSON/xmltojson.cpp
+++ XMLtoJSON/xmltojson.cpp
@@ -2,11 +2,40 @@
 #include "./rapidjson/writer.h"
 #include "./rapidjson/stringbuffer.h"
 #include "./tinyxml2.h"
+#include <string>
 
-using namespace rapidjson;
 using namespace std;
 
+/*
+    Convert XML String to JSON String
+    @param xmlString XML content as a string
+    @return JSON content as a string
+*/
 string xmlToJson(string xmlString) {
+    // convert string to xml document
+    tinyxml2::XMLDocument doc;
+    if (doc.Parse(xmlString.c_str()) != tinyxml2::XML_SUCCESS) {
+        return "Invalid XML Text";
+    }
+
+
+    rapidjson::Document jsonDoc;
+    jsonDoc.SetObject();
+
+    // Iterate over XML tags
+    for (const tinyxml2::XMLNode* node = doc.FirstChild(); node != nullptr; node = node->NextSibling()) {
+        if (node->ToElement()) {
+            rapidjson::Value jsonElement(rapidjson::kObjectType);
+            // Iterate over tag attributes
+            for (const tinyxml2::XMLAttribute* attribute = node->ToElement()->FirstAttribute(); attribute != nullptr; attribute == attribute->Next()) {
+                jsonElement.AddMember(rapidjson::Value().SetString(attribute->Name()), jsonElement, jsonDoc.GetAllocator()); // TODO: fix IntelliSense warning here
+            }
+            {
+                /* code */
+            }
+            
+        }
+    }
     // TODO
     return "WIP";
 }
