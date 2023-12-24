b09be9c24111c2417202e78b6cf40f45eb7c0e40
Omar Emad <omarelwardany@outlook.com>
2023-12-24 23:14:35 +0200

    added Tree classes

---
+++ Structures/Tree.h
@@ -0,0 +1,57 @@
+#include <vector>
+
+class TreeNode
+{
+private:
+public:
+    const char *name;
+    const char *value;
+    std::vector<TreeNode*> children;
+    TreeNode* next_sibling;
+    TreeNode* parent;
+    TreeNode(const char* name, const char* value, TreeNode* parent);
+    ~TreeNode();
+    void add_child(const char* name, const char* value);
+};
+
+TreeNode::TreeNode(const char* name, const char* value, TreeNode* parent)
+    : name(name), value(value), parent(parent), next_sibling(nullptr)
+{
+        if (parent)
+            parent->children.push_back(this);
+}
+
+TreeNode::~TreeNode()
+{
+    for (TreeNode* child:children) {
+        delete child;
+    }
+}
+
+void TreeNode::add_child(const char* name, const char* value) {
+    TreeNode* new_node = new TreeNode(name,value,this);
+    this->children.back()->next_sibling = new_node;
+    this->children.push_back(new_node);
+}
+
+
+class Tree
+{
+private:
+    
+public:
+    TreeNode* root;
+    Tree(TreeNode* root);
+    ~Tree();
+};
+
+
+Tree::Tree(TreeNode* root)
+{
+    this->root = root;
+}
+
+Tree::~Tree()
+{
+    delete root;
+}
