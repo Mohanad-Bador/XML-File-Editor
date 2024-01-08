45a4884119e78615e814c1309dc1cf7e129bd5d8
Mohammed Magdy <130768316+MohammedMagdyy@users.noreply.github.com>
2024-01-08 13:15:30 +0200

    Update MyUniqueCompression.h

--- Compressing&Decompressing(Magdy)/MyUniqueCompression.h
+++ Compressing&Decompressing(Magdy)/MyUniqueCompression.h
@@ -1,9 +1,12 @@
-#pragma once
+#ifndef MyUniqueCompression_h
+#define MyUniqueCompression_h
+
 
 #include <iostream>
 #include <string>
 #include <queue>
 #include <unordered_map>
+
 
 struct HuffNode {
     char huffCharacter;
@@ -14,6 +17,7 @@ struct HuffNode {
     HuffNode(char Character, int Frequency) : huffCharacter(Character), huffFrequency(Frequency), huffLeft(nullptr), huffRight(nullptr) {}
 };
 
+
 struct CompareHuff {
     bool operator()(HuffNode* L, HuffNode* R) const {
         if (L->huffFrequency == R->huffFrequency)
@@ -22,26 +26,47 @@ struct CompareHuff {
     }
 };
 
+
 class MyUniqueCompression {
+
 private:
-    HuffNode* huffRoot;
+     HuffNode* huffRoot;
+
     std::string compressedText;
 
     HuffNode* buildHuffT(std::priority_queue<HuffNode*, std::vector<HuffNode*>, CompareHuff>& X);
+
     void generateHuffCode(HuffNode* HR, std::string code, std::unordered_map<char, std::string>& huffCodes);
+
     std::string encoding(std::string inputData, std::unordered_map<char, std::string>& huffCodes);
+
     char binaryStringToChar(const std::string& binaryString);
+
     std::string byteToChar(std::string data);
 
+
 public:
+
     MyUniqueCompression();
+
     ~MyUniqueCompression();
+
     std::string compressData(std::string inputText);
+
     std::string decompressData(const std::string& encodedText);
+
     void setCompress(std::string compressedData);
+
     std::string getCompress();
+
     HuffNode* getHuffRoot();
+
     std::string charToBinaryStr(const std::string& text);
+
     std::string Decoding(const std::string& encodedData, HuffNode* root);
+
     void deleteHuffTree(HuffNode* huffNode);
+
 };
+
+#endif
